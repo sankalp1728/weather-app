962db3859429fcf8aed7af83f4004a97089a61ca
Sankalp <sankalp1728@gmail.com>
2020-02-11 16:18:54 +0530

    some name changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/fd995c81b5ecbe7b81b24a2e4f19a420/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/fd995c81b5ecbe7b81b24a2e4f19a420/' + latitude + ',' + longitude + 'units=si'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress celcius out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
